3eadd08919a75336f7b4cc6886be3237e8ffaea8
ye win <yewin.aung@zigway.co>
2022-02-22 10:18:03 +0700

    remove couverage & add stylelint config

--- webpack.config.js
+++ webpack.config.js
@@ -36,6 +36,10 @@ module.exports = {
           },
         ],
       },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
     ],
   },
   plugins: [
